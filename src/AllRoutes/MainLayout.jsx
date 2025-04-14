bc793270a0479225806cd1222c7ef60db4f2ea0f
Shakil Ahmed <itsdevelopershakil@gmail.com>
2025-04-14 19:33:12 +0600

    dashboard design begins

--- src/AllRoutes/MainLayout.jsx
+++ src/AllRoutes/MainLayout.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { Outlet, ScrollRestoration } from "react-router";
+import DHeader from "../components/partials/DHeader";
+import Sidebar from "../components/partials/Sidebar";
 
 const MainLayout = () => {
-    return (
-        <>
-            {/* <Header />
-            <Navbar /> */}
-            <Outlet />
-            <ScrollRestoration />
-            {/* <Footer /> */}
-        </>
-    );
+  return (
+    <main className="bg-[#EAEAEA] h-screen w-full flex flex-col">
+      <DHeader />
+      {/* <Navbar /> */}
+      <div className="flex size-full">
+        <Sidebar />
+        <section className="w-full">
+          <Outlet />
+        </section>
+      </div>
+      <ScrollRestoration />
+      {/* <Footer /> */}
+    </main>
+  );
 };
 
 export default MainLayout;
