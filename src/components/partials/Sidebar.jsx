a57a4cfb4caeb2995e537073fc16e31e80f21fdd
Shakil Ahmed <itsdevelopershakil@gmail.com>
2025-04-15 14:28:47 +0600

    dashboard header done

--- src/components/partials/Sidebar.jsx
+++ src/components/partials/Sidebar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Logo from "../../assets/images/flytechBlack.png";
 
 const Sidebar = () => {
   return (
-    <div className="w-[240px] shrink-0 h-full bg-[#F7F7F7] rounded-2xl">
-      Sidebar
+    <div className="w-[240px] shrink-0 h-full bg-secondary rounded-2xl">
+      <div className="flex justify-center p-5">
+        <img src={Logo} alt="Flytech IT Logo" className="w-[120px]" />
+      </div>
     </div>
   );
 };
