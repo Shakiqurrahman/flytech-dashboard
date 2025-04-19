8230bd4d17df1f2dd4b33e77f316dbc8e3be81c0
Shakil Ahmed <itsdevelopershakil@gmail.com>
2025-04-19 16:52:47 +0600

    inbox page design done

--- src/pages/inbox/InboxPage.jsx
+++ src/pages/inbox/InboxPage.jsx
@@ -1,7 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
+import { GoTrash } from "react-icons/go";
+import { Link } from "react-router";
 
 const InboxPage = () => {
-  return <div>InboxPage</div>;
+  const [messages, setMessages] = useState([
+    {
+      id: 1,
+      name: "Shakil Ahmed",
+      subject: "Full Time UI Designer - Judha Maygustya",
+      description: `Dear Mrs Azie Melasar, I am Judha Maygustya, writing to Dear Mrs
+            Azie Melasar, I am Judha Maygustya, writing to Dear Mrs Azie
+            Melasar, I am Judha Maygustya, writing to Dear Mrs Azie Melasar, I
+            am Judha Maygustya, writing to`,
+      isSelected: false,
+    },
+    {
+      id: 2,
+      name: "Mahdi Hussain",
+      subject: "Full Time UI Designer - Judha Maygustya",
+      description: `Dear Mrs Azie Melasar, I am Judha Maygustya, writing to Dear Mrs
+            Azie Melasar, I am Judha Maygustya, writing to Dear Mrs Azie
+            Melasar, I am Judha Maygustya, writing to Dear Mrs Azie Melasar, I
+            am Judha Maygustya, writing to`,
+      isSelected: false,
+    },
+    {
+      id: 3,
+      name: "Mokbul Hussain",
+      subject: "Full Time UI Designer - Judha Maygustya",
+      description: `Dear Mrs Azie Melasar, I am Judha Maygustya, writing to Dear Mrs
+            Azie Melasar, I am Judha Maygustya, writing to Dear Mrs Azie
+            Melasar, I am Judha Maygustya, writing to Dear Mrs Azie Melasar, I
+            am Judha Maygustya, writing to`,
+      isSelected: false,
+    },
+    {
+      id: 4,
+      name: "Shakiqur Rahman",
+      subject: "Full Time UI Designer - Judha Maygustya",
+      description: `Dear Mrs Azie Melasar, I am Judha Maygustya, writing to Dear Mrs
+            Azie Melasar, I am Judha Maygustya, writing to Dear Mrs Azie
+            Melasar, I am Judha Maygustya, writing to Dear Mrs Azie Melasar, I
+            am Judha Maygustya, writing to`,
+      isSelected: false,
+    },
+  ]);
+
+  const handleChecked = (e, id) => {
+    setMessages((prev) =>
+      prev.map((msg, i) =>
+        i === id ? { ...msg, isSelected: e.target.checked } : msg
+      )
+    );
+  };
+
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="text-2xl font-semibold">Inbox</h1>
+          <p className="text-sm">Display all the contact form messages list.</p>
+        </div>
+        <div className="flex gap-4 items-center">
+          <button
+            type="button"
+            className="flex gap-1 items-center border border-gray-500 py-2 px-4 rounded-md cursor-pointer hover:text-primary hover:border-primary duration-300"
+          >
+            <GoTrash />
+          </button>
+        </div>
+      </div>
+      <div className="mt-8">
+        {messages.map((message, i) => (
+          <div
+            key={i}
+            className={`flex items-center gap-5 py-2 border-b border-gray-200 first:border-t ${
+              message.isSelected ? "bg-primary/5" : ""
+            }`}
+          >
+            <input
+              type="checkbox"
+              id={i}
+              className="shrink-0 cursor-pointer"
+              checked={message.isSelected}
+              onChange={(e) => handleChecked(e, i)}
+            />
+            <Link to={"read"} state={message} className="flex items-center">
+              <div className="flex items-center gap-3 shrink-0 mr-5">
+                <div className="flex items-center justify-center text-lg font-semibold rounded bg-primary/20 size-8 shrink-0">
+                  {message.name.charAt(0).toUpperCase()}
+                </div>
+                <h1 className="font-medium">{message.name}</h1>
+              </div>
+              <div className="flex items-center gap-2">
+                <h1 className="shrink-0">{message.subject}</h1>
+                <p className="line-clamp-1 text-gray-400 font-extralight">
+                  - {message.description}
+                </p>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default InboxPage;
--- src/pages/team/TeamPage.jsx
+++ src/pages/team/TeamPage.jsx
@@ -1,99 +1,106 @@
 import React, { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { FiEdit3 } from "react-icons/fi";
+import { GoTrash } from "react-icons/go";
+import { Link } from "react-router";
 import img1 from "../../../public/images/ceo.webp";
 import img2 from "../../../public/images/coo.webp";
 import img3 from "../../../public/images/cto.webp";
-import { Link } from "react-router";
-import { FiEdit3 } from "react-icons/fi";
-import { GoTrash } from "react-icons/go";
-import { FaPlus } from "react-icons/fa";
 
 const TeamPage = () => {
-    const data = [
-        {
-            name: "Rare Al Samir",
-            position: "Founder & CEO",
-            desck: "Ex-Bangladesh Army, 3 years of Offline Education Business Experience, 5 years of Teaching Experience Finance Graduate",
-            img: img1,
-        },
-        {
-            name: "Jamila Bupasha Khushbu",
-            position: "Co-Founder & COO",
-            desck: "Economics Post Graduate, Ex-BYLC Graduate, 6 years of Experience in Education Sector, Mentored 5000+ students at s@ifurs & two other educational institution, Lead an NGO for 4 years as an Operation Lead",
-            img: img2,
-        },
-        {
-            name: "Tanveer Hossain Munim",
-            position: "CTO",
-            desck: "Software Engineer, 5 years of Teaching Experience to 50,000+ Students",
-            img: img3,
-        },
-        {
-            name: "Rare Al Samir",
-            position: "Founder & CEO",
-            desck: "Ex-Bangladesh Army, 3 years of Offline Education Business Experience, 5 years of Teaching Experience Finance Graduate",
-            img: img1,
-        },
-        {
-            name: "Jamila Bupasha Khushbu",
-            position: "Co-Founder & COO",
-            desck: "Economics Post Graduate, Ex-BYLC Graduate, 6 years of Experience in Education Sector, Mentored 5000+ students at s@ifurs & two other educational institution, Lead an NGO for 4 years as an Operation Lead",
-            img: img2,
-        },
-        {
-            name: "Tanveer Hossain Munim",
-            position: "CTO",
-            desck: "Software Engineer, 5 years of Teaching Experience to 50,000+ Students",
-            img: img3,
-        },
-    ];
+  const data = [
+    {
+      name: "Rare Al Samir",
+      position: "Founder & CEO",
+      desck:
+        "Ex-Bangladesh Army, 3 years of Offline Education Business Experience, 5 years of Teaching Experience Finance Graduate",
+      img: img1,
+    },
+    {
+      name: "Jamila Bupasha Khushbu",
+      position: "Co-Founder & COO",
+      desck:
+        "Economics Post Graduate, Ex-BYLC Graduate, 6 years of Experience in Education Sector, Mentored 5000+ students at s@ifurs & two other educational institution, Lead an NGO for 4 years as an Operation Lead",
+      img: img2,
+    },
+    {
+      name: "Tanveer Hossain Munim",
+      position: "CTO",
+      desck:
+        "Software Engineer, 5 years of Teaching Experience to 50,000+ Students",
+      img: img3,
+    },
+    {
+      name: "Rare Al Samir",
+      position: "Founder & CEO",
+      desck:
+        "Ex-Bangladesh Army, 3 years of Offline Education Business Experience, 5 years of Teaching Experience Finance Graduate",
+      img: img1,
+    },
+    {
+      name: "Jamila Bupasha Khushbu",
+      position: "Co-Founder & COO",
+      desck:
+        "Economics Post Graduate, Ex-BYLC Graduate, 6 years of Experience in Education Sector, Mentored 5000+ students at s@ifurs & two other educational institution, Lead an NGO for 4 years as an Operation Lead",
+      img: img2,
+    },
+    {
+      name: "Tanveer Hossain Munim",
+      position: "CTO",
+      desck:
+        "Software Engineer, 5 years of Teaching Experience to 50,000+ Students",
+      img: img3,
+    },
+  ];
 
-    const [teamData, setTeamData] = useState(data);
+  const [teamData, setTeamData] = useState(data);
 
-    return (
+  return (
+    <div>
+      <div className="flex items-center justify-between">
         <div>
-            <div className="flex items-center justify-between">
-                <div>
-                    <h1 className="text-2xl font-semibold">Edit Faqs</h1>
-                </div>
-                <div className="flex gap-4 items-center">
-                    <Link
-                        to="create"
-                        className="flex gap-1 items-center border border-gray-500 py-2 px-4 rounded-md cursor-pointer hover:text-primary hover:border-primary duration-300"
-                    >
-                        <FaPlus /> Add
-                    </Link>
-                </div>
-            </div>
-            <div className="grid grid-cols-4 gap-4 pt-10">
-                {teamData.map((v, i) => (
-                    <div
-                        className="flex items-center flex-col text-center bg-white rounded-md px-2 py-4 border border-primary cursor-pointer relative  overflow-hidden group"
-                        key={i}
-                    >
-                        <div className="absolute right-[12px] top-[12px] flex flex-col gap-2 translate-x-[150%] group-hover:translate-x-0 duration-300">
-                            <Link
-                                to="edit"
-                                className="hover:bg-primary border border-primary hover:text-white p-2 rounded-lg"
-                            >
-                                <FiEdit3 className="size-4" />
-                            </Link>
-                            <button className="hover:bg-primary border border-primary hover:text-white p-2 rounded-lg cursor-pointer">
-                                <GoTrash className="size-4" />
-                            </button>
-                        </div>
-                        <img
-                            src={v.img}
-                            alt=""
-                            className="size-40 object-contain rounded-full cursor-pointer"
-                        />
-                        <h3 className="text-xl font-semibold mt-4">{v.name}</h3>
-                        <p className="font-medium text-primary">{v.position}</p>
-                        <p className="mt-1 text-xs text-gray-500">{v.desck}</p>
-                    </div>
-                ))}
-            </div>
+          <h1 className="text-2xl font-semibold">Team</h1>
+          <p className="text-sm">Display all the team members list.</p>
         </div>
-    );
+        <div className="flex gap-4 items-center">
+          <Link
+            to="create"
+            className="flex gap-1 items-center border border-gray-500 py-2 px-4 rounded-md cursor-pointer hover:text-primary hover:border-primary duration-300"
+          >
+            <FaPlus /> Add
+          </Link>
+        </div>
+      </div>
+      <div className="grid grid-cols-4 gap-4 pt-10">
+        {teamData.map((v, i) => (
+          <div
+            className="flex items-center flex-col text-center bg-white rounded-md px-2 py-4 border border-primary cursor-pointer relative  overflow-hidden group"
+            key={i}
+          >
+            <div className="absolute right-[12px] top-[12px] flex flex-col gap-2 translate-x-[150%] group-hover:translate-x-0 duration-300">
+              <Link
+                to="edit"
+                className="hover:bg-primary border border-primary hover:text-white p-2 rounded-lg"
+              >
+                <FiEdit3 className="size-4" />
+              </Link>
+              <button className="hover:bg-primary border border-primary hover:text-white p-2 rounded-lg cursor-pointer">
+                <GoTrash className="size-4" />
+              </button>
+            </div>
+            <img
+              src={v.img}
+              alt=""
+              className="size-40 object-contain rounded-full cursor-pointer"
+            />
+            <h3 className="text-xl font-semibold mt-4">{v.name}</h3>
+            <p className="font-medium text-primary">{v.position}</p>
+            <p className="mt-1 text-xs text-gray-500">{v.desck}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TeamPage;
